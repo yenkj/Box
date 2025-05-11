cad21d50133d05ce7ee0e5c884306314fbdc3502
yenkj <49618620+yenkj@users.noreply.github.com>
2025-05-12 01:39:56 +0800

    Update PlayActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/PlayActivity.java
@@ -1549,13 +1549,6 @@ public class PlayActivity extends BaseActivity {
 
     ExecutorService parseThreadPool;
 
-    private String encodeUrl(String url) {
-        try {
-            return URLEncoder.encode(url, "UTF-8");
-        } catch (Exception e) {
-            return url;
-        }
-    }
 
     private void doParse(ParseBean pb) {
         stopParse();
